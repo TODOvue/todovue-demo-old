0340300b8a244b385da9745fd6d191bbb28fdcc0
Cristhian Daza <cristian.d.b@hotmail.com>
2023-05-01 21:02:18 -0500

    fix(*) build is corrected

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import TvDemo from "./component/TvDemo.vue";
 import "vue-highlight-code/dist/style.css";
-import "@/assets/scss/_global.scss";
 
 createApp(TvDemo).mount("#todovue-demo");
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,4 @@
-module.exports = {
-  css: {
-    extract: {
-      filename: "[name].css",
-      chunkFilename: "[name].css",
-    },
-  },
-  configureWebpack: {
-    entry: "./src/component/TvDemo.vue",
-    output: {
-      filename: "tvdemo.min.js",
-      library: "TvDemo",
-      libraryExport: "default",
-      libraryTarget: "umd",
-      umdNamedDefine: true,
-    },
-    externals: {
-      vue: "vue",
-    },
-  },
-};
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+});
