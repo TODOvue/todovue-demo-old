f55a872c441da47faeed369541e8a3cdb72bd68a
Cristhian Daza <cristian.d.b@hotmail.com>
2023-05-03 14:12:08 -0500

    test(tv-demo.spec.js - tv-demo.spec.js.snap) Unit tests are added

--- tests/unit/component/tv-demo.spec.js
+++ tests/unit/component/tv-demo.spec.js
@@ -1,9 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
 import TvDemo from "@/component/TvDemo.vue";
+import { onMounted, ref } from "vue";
 
 describe("TvDemo Component", () => {
   describe("Test in template", () => {
-    // eslint-disable-next-line no-unused-vars
     let wrapper;
     beforeEach(() => {
       wrapper = shallowMount(TvDemo, {
@@ -33,20 +33,47 @@ describe("TvDemo Component", () => {
     it("matches snapshot", () => {
       const wrapper = shallowMount(TvDemo, {
         props: {
-          invertTheme: false,
           hideBackground: false,
           component: null,
           variants: null,
         },
         data() {
           return {
-            themeInvert: "invert-dark",
             theme: "dark",
             selectedVariantIndex: 0,
           };
         },
       });
       expect(wrapper.html()).toMatchSnapshot();
+    });
+
+    it("Should change the theme to light when changing the select", () => {
+      const select = wrapper.find("select");
+      select.setValue("light");
+      expect(wrapper.vm.theme).toBe("light");
+    });
+
+    it("Should change the theme to dark when changing the select", () => {
+      const select = wrapper.find("select");
+      select.setValue("dark");
+      expect(wrapper.vm.theme).toBe("dark");
+    });
+
+    it("Should return null when not finding the localStorage", () => {
+      const localStorageSpy = jest.spyOn(
+        window.localStorage.__proto__,
+        "getItem"
+      );
+      localStorageSpy.mockReturnValue("dark");
+
+      const theme = ref(null);
+      onMounted(() => {
+        if (localStorage.getItem("theme")) {
+          theme.value = localStorage.getItem("theme");
+        }
+      });
+
+      expect(theme.value).toBe(null);
     });
   });
 });
